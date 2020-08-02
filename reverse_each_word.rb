45cb15dafc3c8bee0aee86ad7641dda108e6e158
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-02 21:11:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,5 +10,8 @@ end
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
-  
+  array.collect do |string|
+    test_array << string.reverse
+  end
+  test_array.join(" ")
   
